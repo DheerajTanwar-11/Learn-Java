01dba4bcae2138576d2808ee93cf3ec85b608df8
Dheeraj Tanwar <dheerajtanwar640@gmail.com>
2023-09-24 12:56:28 +0000

    firstClass(Introduction)

---
+++ 01_Basics/firstClass.java
@@ -0,0 +1,10 @@
+// To Start with java coding we have to start with a fixed java template given below....
+
+class firstClass{
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }
+}
+
+/*  Q. How the code in running in java => 
+In java we have two steps for running code: Compilation and Execution. Our source code file with extension of .java goes into compiler which is already available in JDK. Compiler convert this source code into byte code with extension if .class. Now this byte code goes into Java Virtual Machine where this code convert into native code which is understandable by Machines. */
