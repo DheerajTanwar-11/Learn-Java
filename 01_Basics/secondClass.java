a14fb2cbf2af70b5a56fff18371e88ec360e54cf
Dheeraj Tanwar <dheerajtanwar640@gmail.com>
2023-09-24 15:43:46 +0000

    Input_Output_Variable

---
+++ 01_Basics/secondClass.java
@@ -0,0 +1,18 @@
+// Here we will learn input and output in Java.
+// To give output on the console in java we use a specific line 'System.out.print(Enter what you want to print)'. 
+// Here system is a class in java.
+class secondClass{
+    public static void main(String args[]){
+        // Output
+        System.out.print("Hi! My name is Dheeraj Tanwar");// This method will not give us new line after printing this line.
+        // Output with New Line
+        System.out.println("Hi! My name is Dheeraj Tanwar");
+        System.out.print("I am learning Java.\nI love eSports.\n");
+        //Question time (Print Star Pattern)
+        System.out.println("###########################");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
