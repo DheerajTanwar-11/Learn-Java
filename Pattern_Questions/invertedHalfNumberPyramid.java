1ca1357e2b3c3e79534351d86ff94958d659dfef
Dheeraj Tanwar <dheerajtanwar640@gmail.com>
2023-09-26 12:44:36 +0000

    invertedHalfNumberPyramid

---
+++ Pattern_Questions/invertedHalfNumberPyramid.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class halfPyramid180deg{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        sc.close();
+        for(int i = rows; i >= 1; i--){
+            for(int j = 1; j <= i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
