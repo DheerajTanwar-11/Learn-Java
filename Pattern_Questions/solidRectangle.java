02e6a202e056958b37f70d2613e6c0946c98a695
Dheeraj Tanwar <dheerajtanwar640@gmail.com>
2023-09-26 11:37:40 +0000

    Solid Rectangle

---
+++ Pattern_Questions/solidRectangle.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class solidRectangle{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        System.out.print("Enter the number of columns: ");
+        int columns = sc.nextInt();
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < columns; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
