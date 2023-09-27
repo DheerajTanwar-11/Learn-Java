23d4bf07f854badbb30a28c25edbd1ba5caed539
Dheeraj Tanwar <dheerajtanwar640@gmail.com>
2023-09-27 13:26:50 +0000

    question2

---
+++ 02_Functions/question2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class question2{
+    public static int factorial(int number){
+        int result = 1;
+        if(number == 0 || number == 1){
+            return result;
+        } else {
+            for(int i = 1; i <= number; i++){
+                result = result*i;
+            }
+            return result;
+        }
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the input: ");
+        int number = sc.nextInt();
+        sc.close();
+        System.out.println("Factorial of the given input is: " + factorial(number));
+    }
+}
