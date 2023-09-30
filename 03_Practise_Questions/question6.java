35ca89b55ffe87c5a6bcc3308361619f49ba5e0e
Dheeraj Tanwar <dheerajtanwar640@gmail.com>
2023-09-30 04:49:31 +0000

    question6

---
+++ 03_Practise_Questions/question6.java
@@ -0,0 +1,9 @@
+class question6{
+    public static void main(String args[]){
+        int n = 3;
+        do{
+            System.out.println("You have been trapped....");
+            n++;
+        } while(n > 2);
+    }
+}
